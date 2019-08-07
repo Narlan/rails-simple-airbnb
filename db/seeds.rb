17f357b9b1c4409cf30ba5deb25b83500580e37e
Adam Castro Guzman <castroguzman033@gmail.com>
2019-08-07 15:55:18 +0200

    In progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.delete_all
+count = 0
+times = 500
+
+times.times do
+  Flat.create!(
+    name: Faker::Lorem.sentence(word_count: 2, supplemental: false, random_words_to_add: 2),
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence(word_count: 10, supplemental: false, random_words_to_add: 15),
+    price_per_night: rand(10..120),
+    number_of_guests: rand(1..6)
+  )
+  count += 1
+  puts "#{count}/#{times}"
+end
